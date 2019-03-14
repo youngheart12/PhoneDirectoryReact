fc85875465b7e57ccf3a4fb3b25f1826dc512a9a
youngheart12 <manishkumar199222@gmail.com>
2019-03-14 08:23:03 +0530

    Updates

---
+++ precache-manifest.9a6abab650e51902e95317623b89240f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4d2465b1c928d556015f",
+    "url": "/static/js/main.e5e0a98a.chunk.js"
+  },
+  {
+    "revision": "818b894d34dcf3be2e0f",
+    "url": "/static/js/2.428cd9a3.chunk.js"
+  },
+  {
+    "revision": "4d2465b1c928d556015f",
+    "url": "/static/css/main.5a876d00.chunk.css"
+  },
+  {
+    "revision": "35524219a6fcca4f26c3edbd8f4e43cf",
+    "url": "/index.html"
+  }
+];
